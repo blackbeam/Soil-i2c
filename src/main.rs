9c84229741b97594030683629b2d9b265939ae7f
Cole Heslinga <cole.heslinga@gmail.com>
2020-08-04 22:25:33 -0400

    Redoing the last changes, here's a clean build

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,4 @@
 mod soil_lib;
-extern crate mysql;
-
-mod data_mysql {
-    pub fn insert(plnum: u8, tempwrite: f32, moistwrite: u16) -> Result<(), mysql::Error> {
-        use mysql::*;
-        use mysql::prelude::*;
-
-        let url = "PUT YOUR MYSQL SERVER URL IN HERE (I'll make the program use a config file for this later)";
-        let pool = Pool::new(url)?;
-        let mut conn = pool.get_conn()?;
-   
-        let plantno = format!("Plant {}", plnum);
-        
-        conn.exec_drop(
-            r"INSERT INTO SoilData (Plant,Readtime,Moisture,Temperature) VALUES (?)", (plantno, "CURRENT_TIME", moistwrite, tempwrite)
-        )?;
-
-        Ok(())
-    }
-}
 
 pub use crate::soil_lib::i2conn;
 pub use crate::soil_lib::stemconn;
@@ -38,9 +18,6 @@ fn mx_channel(channel: u8) {
 //port in the range of my constraints, fetching both the temperature and 
 //capacitance values at a 500ms delay, and prints them both out on one line.
 //
-//Recently added: Now it writes this stuff to a MySQL database! I'll document
-//it when I'm less busy/lazy.
-//
 fn main() {
     let mut temp: f32;
     let mut cap: u16;
@@ -57,11 +34,6 @@ fn main() {
             temp = stemconn::sensetemp(500);
             cap = stemconn::sensecap(500);
             println!("Temperature: {}\nCapacitance: {}\n", temp, cap);
-            println!("Writing to mysql database...");
-            match data_mysql::insert(i, temp, cap) {
-                Ok(m) => m,
-                Err(err) => println!("Error writing to mysql!: {}", err),
-            }
         }
     }
 }
