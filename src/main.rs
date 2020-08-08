bc33627ffada3c98a77dde7fb43b01b9ce855dd3
Cole Heslinga <cole.heslinga@gmail.com>
2020-08-08 11:48:48 -0400

    Added mysql functionality back in. Working with a local server for now, IP not working.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
+extern crate mysql;
+
 mod soil_lib;
 
 pub use crate::soil_lib::i2conn;
 pub use crate::soil_lib::stemconn;
 
+mod data_mysql {
+    pub fn insert(plnum: u8, tempwrite: f32, moistwrite: u16) -> Result<(), mysql::Error> {
+        use mysql::*;
+        use mysql::prelude::*;
+
+        let url = "mysql://soil:Test1234@localhost:3307/SoilTest1";
+        let pool = Pool::new(url)?;
+        let mut conn = pool.get_conn()?;
+   
+        let plantno = format!("Plant {}", plnum);
+        
+        conn.exec_drop(
+            r"INSERT INTO SoilData (Plant,Readtime,Moisture,Temperature) VALUES (?,CURRENT_TIME,?,?)", (plantno, moistwrite, tempwrite)
+        )?;
+
+        Ok(())
+    }
+}
 //mx_channel: This is basically just an alias for the i2conn::muxer function that 
 //handles the error. (Look at me, handling errors like a normal programmer :P)
 //
@@ -34,6 +54,12 @@ fn main() {
             temp = stemconn::sensetemp(500);
             cap = stemconn::sensecap(500);
             println!("Temperature: {}\nCapacitance: {}\n", temp, cap);
+            
+            println!("Writing to MySQL database...");
+            match data_mysql::insert(i, temp, cap) {
+               Ok(m) => m,
+               Err(err) => println!("Error writing to database!!! {}", err),
+            }
         }
     }
 }
